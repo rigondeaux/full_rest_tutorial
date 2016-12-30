d3bf9dfbdbbd1f926f461ecdd9a3832d3157b947
Rigondeaux <rigondeaux@gmail.com>
2016-12-31 00:56:42 +1100

    Started chapter 1 - Serialization

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
-
+# refactored to use ModelSerializer class
+"""
 class SnippetSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     title = serializers.CharField(required=False, allow_blank=True, max_length=100)
@@ -9,6 +10,12 @@ class SnippetSerializer(serializers.Serializer):
     linenos = serializers.BooleanField(required=False)
     language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
     style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
+"""
+
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
 
     def create(self, validated_data):
         """
